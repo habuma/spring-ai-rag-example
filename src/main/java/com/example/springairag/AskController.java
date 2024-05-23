2f254ebbb6aa702d4847e4f45b46f41c745b8319
Craig Walls <craig@habuma.com>
2024-05-22 20:07:48 -0600

    Update to use fluent ChatClient

--- src/main/java/com/example/springairag/AskController.java
+++ src/main/java/com/example/springairag/AskController.java
@@ -1,9 +1,8 @@
 package com.example.springairag;
 
+import java.util.List;
+
 import org.springframework.ai.chat.ChatClient;
-import org.springframework.ai.chat.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -13,10 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/ask")
@@ -36,16 +31,22 @@ public class AskController {
 
     @PostMapping
     public Answer ask(@RequestBody Question question) {
-        List<Document> similarDocuments = vectorStore.similaritySearch(SearchRequest.query(question.question()).withTopK(2));
-        List<String> contentList = similarDocuments.stream().map(Document::getContent).toList();
-        PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
-        Map<String, Object> promptParameters = new HashMap<>();
-        promptParameters.put("input", question.question());
-        promptParameters.put("documents", String.join("\n", contentList));
-        Prompt prompt = promptTemplate.create(promptParameters);
+        List<Document> similarDocuments = vectorStore
+            .similaritySearch(SearchRequest.query(question.question())
+            .withTopK(2));
+        List<String> contentList = similarDocuments.stream()
+            .map(Document::getContent)
+            .toList();
 
-        ChatResponse response = aiClient.call(prompt);
-        return new Answer(response.getResult().getOutput().getContent());
+        String answer = aiClient.prompt()
+            .user(userSpec -> userSpec
+                .text(ragPromptTemplate)
+                .param("input", question.question())
+                .param("documents", String.join("\n", contentList)))
+            .call()
+            .content();
+
+        return new Answer(answer);
     }
 
 }
--- src/main/java/com/example/springairag/SpringAIRAGExample.java
+++ src/main/java/com/example/springairag/SpringAIRAGExample.java
@@ -1,22 +1,21 @@
 package com.example.springairag;
 
-import org.apache.commons.lang3.StringUtils;
+import java.io.File;
+import java.util.List;
+
 import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatModel;
 import org.springframework.ai.document.Document;
-import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
-
-import java.io.File;
-import java.util.List;
 
 @SpringBootApplication
 public class SpringAIRAGExample {
@@ -32,8 +31,13 @@ public class SpringAIRAGExample {
     private Resource pdfResource;
 
     @Bean
-    SimpleVectorStore simpleVectorStore(EmbeddingClient embeddingClient) {
-        SimpleVectorStore simpleVectorStore = new SimpleVectorStore(embeddingClient);
+    ChatClient chatClient(ChatModel chatModel) {
+        return ChatClient.builder(chatModel).build();
+    }
+
+    @Bean
+    SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel) {
+        SimpleVectorStore simpleVectorStore = new SimpleVectorStore(embeddingModel);
         File vectorStoreFile = new File(vectorStorePath);
         if (vectorStoreFile.exists()) { // load existing vector store if exists
             simpleVectorStore.load(vectorStoreFile);
